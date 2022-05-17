4f571df1e343d03da8c81e7d1e2654c733a3408b
dulajsan123 <dulajs@codegen.net>
2022-05-17 22:02:43 +0530

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,19 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-   items = [
-    { title: 'Slide 1' },
-    { title: 'Slide 2' },
-    { title: 'Slide 3' },
-  ]
+export class AppComponent {
+  items = [{ title: 'dulaj 1' }, { title: 'Slide 2' }, { title: 'Slide 3' }];
 
   addSlide() {
     this.items.push({
-      title: `Slide 4`
+      title: `Slide 4`,
     });
   }
-
 }
